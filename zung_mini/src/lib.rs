f3a3f68935f2d42f5a5472e37cee12fb75fd96d8
Ishaan Goel <ishaangoel.99@gmail.com>
2024-09-30 18:26:29 +0530

    feat: Added more usage features to progbar cli

--- zung_mini/src/lib.rs
+++ zung_mini/src/lib.rs
@@ -19,6 +19,28 @@ pub struct MiniArgs {
 enum MiniCommands {
     /// Print a progress bar to an iterator.
     Progbar {
+        #[command(subcommand)]
+        command: ProgBarCommands,
+    },
+}
+
+#[derive(Clone, Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+enum ProgBarCommands {
+    UnBounded {
+        #[arg(short, long, default_value_t = String::from("#"))]
+        bar_style: String,
+    },
+    Bounded {
+        #[arg(long, default_value_t = String::from("["))]
+        delim_start: String,
+
+        #[arg( long, default_value_t = String::from("]"))]
+        delim_close: String,
+
+        #[arg(long, default_value_t = String::from("#"))]
+        bar_style: String,
+
         #[arg(short, long, default_value_t = 50)]
         iter_count: u8,
     },
@@ -27,22 +49,32 @@ enum MiniCommands {
 impl MiniArgs {
     pub fn run(self) {
         match self.command {
-            MiniCommands::Progbar { iter_count } => {
+            MiniCommands::Progbar { command } => {
                 use std::thread::sleep;
                 use std::time::Duration;
 
-                // test run UnBounded
-                for _ in (0..iter_count).progbar() {
-                    sleep(Duration::from_millis(50))
-                }
-
-                // test run Bounded
-                for _ in (0..iter_count)
-                    .progbar()
-                    .bar_style('=')
-                    .with_bounds('(', ')')
-                {
-                    sleep(Duration::from_millis(50))
+                match command {
+                    ProgBarCommands::UnBounded { bar_style } => {
+                        // test run UnBounded
+                        for _ in (0..).progbar().bar_style(bar_style) {
+                            sleep(Duration::from_millis(50))
+                        }
+                    }
+                    ProgBarCommands::Bounded {
+                        delim_start,
+                        delim_close,
+                        bar_style,
+                        iter_count,
+                    } => {
+                        // test run Bounded
+                        for _ in (0..iter_count)
+                            .progbar()
+                            .bar_style(bar_style)
+                            .with_bounds(delim_start, delim_close)
+                        {
+                            sleep(Duration::from_millis(50))
+                        }
+                    }
                 }
             }
         }
