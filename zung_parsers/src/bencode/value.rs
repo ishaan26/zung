a05c2efd45f33c876a97de9558d7f125b0a5e8de
Ishaan Goel <ishaangoel.99@gmail.com>
2024-10-09 23:23:01 +0530

    fix: docs

--- zung_parsers/src/bencode/value.rs
+++ zung_parsers/src/bencode/value.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Display};
 
 /// Reprasents Bencode values as a rust enum.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -46,5 +46,38 @@ impl<'a, const N: usize> From<&'a [u8; N]> for ValueInput<'a> {
 impl<'a> From<&'a Vec<u8>> for ValueInput<'a> {
     fn from(b: &'a Vec<u8>) -> Self {
         ValueInput::Bytes(b)
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Null => write!(f, "Null"),
+            Value::Integer(i) => write!(f, "{i}"),
+            Value::Bytes(bytes) => match std::str::from_utf8(bytes) {
+                Ok(s) => write!(f, "{s}"),
+                Err(_) => write!(f, "/*BYTES*/"),
+            },
+            Value::String(s) => write!(f, "{s}"),
+            Value::List(list) => {
+                let mut benstr = String::new();
+                for (i, bencode) in list.iter().enumerate() {
+                    if i != list.len() - 1 {
+                        benstr.push_str(&format!("{}, ", bencode));
+                    } else {
+                        benstr.push_str(&bencode.to_string())
+                    }
+                }
+                write!(f, "[{benstr}]")
+            }
+            Value::Dictionary(dictionary) => {
+                let mut benstr = String::new();
+                for (k, v) in dictionary {
+                    benstr.push_str(&format!("{k} : "));
+                    benstr.push_str(&v.to_string());
+                }
+                write!(f, "[{benstr}]")
+            }
+        }
     }
 }
